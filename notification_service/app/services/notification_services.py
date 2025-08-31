bb340130ca3f9a67cbe6216bb2803a0c53ca8bfb
sfaure <sebastian.faure.l@gmail.com>
2025-08-31 18:21:57 -0400

    feat:  notification service, integrate admin email retrieval

--- notification_service/app/services/notification_services.py
+++ notification_service/app/services/notification_services.py
@@ -1,11 +1,13 @@
 from jinja2 import Environment, FileSystemLoader
 from dotenv import load_dotenv
 from typing import Dict
+import requests
 import resend
 import pika
 import os
 
 from app.schemas.schemas import AuditEvent
+from app.utils.security import create_access_token
 
 load_dotenv()
 
@@ -15,6 +17,10 @@ RABBITMQ_USER = os.getenv("RABBITMQ_USER")
 RABBITMQ_PASS = os.getenv("RABBITMQ_PASS")
 RABBITMQ_QUEUE = os.getenv("RABBITMQ_QUEUE")
 RESEND_API_KEY = os.getenv("RESEND_API_KEY")
+ACCOUNTS_HOST = os.getenv("ACCOUNTS_HOST")
+ACCOUNTS_PORT = os.getenv("ACCOUNTS_PORT")
+ACCOUNTS_PATH = os.getenv("ACCOUNTS_PATH")
+ACCOUNTS_API_URL = f"http://{ACCOUNTS_HOST}:{ACCOUNTS_PORT}/{ACCOUNTS_PATH}?role=admin"
 
 resend.api_key = RESEND_API_KEY
 
@@ -51,10 +57,11 @@ def send_mail(event: AuditEvent) -> Dict:
         changes=event.changes or {},
         timestamp=event.timestamp
     )
-
+    admin_emails = get_admin_emails()
+    print(f"Sending email to: {admin_emails}")
     params: resend.Emails.SendParams = {
         "from": "onboarding@resend.dev",
-        "to": ["sebastian.faure.l@gmail.com"],
+        "to": admin_emails,
         "subject": f"[Audit] {event.action.upper()} on {event.model} record {event.record_id}",
         "html": html_content,
     }
@@ -62,3 +69,21 @@ def send_mail(event: AuditEvent) -> Dict:
     email: resend.Email = resend.Emails.send(params)
 
     return email
+
+
+def get_admin_emails() -> list[str]:
+    access_token = create_access_token(
+        subject="admin_account@example.com",
+        extra_claims={"role": "admin"}
+    )
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+
+    response = requests.get(ACCOUNTS_API_URL, headers=headers)
+    response.raise_for_status()
+    accounts = response.json()
+    return [
+        account["email"]
+        for account in accounts["data"]
+    ]
